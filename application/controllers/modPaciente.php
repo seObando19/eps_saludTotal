78410515523418f17f9eedd88835272fde2d7207
sebastian <sebastiano-10@hotmail.com>
2019-05-16 20:09:47 -0500

    actualizacion modulo paciente

--- application/controllers/modPaciente.php
+++ application/controllers/modPaciente.php
@@ -9,10 +9,10 @@ class Modpaciente extends CI_Controller {
         $this->load->library('grocery_CRUD');
 		//instanciar la libreria
         $this->crud=new grocery_CRUD();
-        /*if(!$this->session->userdata('id'))
+        if(!$this->session->userdata('id'))
 	{
 		redirect('login');
-	} */
+	} 
     }
 	public function index()
 	{
@@ -26,20 +26,20 @@ class Modpaciente extends CI_Controller {
 		//cargar la tabla
 		$this->crud->set_table('paciente');
 		$this->crud->set_relation("ciudad","historia_clinica","lugar_nacim");
-		$this->crud->fields('nombre','apellido','telefono','email','direccion','ciudad','identificacion','observaciones');
-		$this->crud->required_fields('nombre','apellido','telefono','email','direccion','ciudad','identificacion');
-		$this->crud->display_as('nombre','Digite nombre');
-		$this->crud->display_as('apellido','Digite apellido');
-		$this->crud->display_as('telefono','Digite telefono');
-		$this->crud->display_as('email','Digite email');
-		$this->crud->display_as('direccion','Digite direccion');
-		$this->crud->display_as('ciudad','Seleccione ciudad');
-		$this->crud->display_as('identidicacion','Ingrese identificacion');
-		$this->crud->display_as('observaciones','Ingrese las observaciones');
-		$this->crud->display_as('lugar_nacim','ciudad');
+		$this->crud->fields("nombre","apellido","telefono","email","direccion","ciudad","identificacion","observaciones");
+		$this->crud->required_fields("nombre","apellido","telefono","email","direccion","ciudad","identificacion");
+		$this->crud->display_as("nombre","Digite nombre");
+		$this->crud->display_as("apellido","Digite apellido");
+		$this->crud->display_as("telefono","Digite telefono");
+		$this->crud->display_as("email","Digite email");
+		$this->crud->display_as("direccion","Digite direccion");
+		$this->crud->display_as("ciudad","Seleccione ciudad");
+		$this->crud->display_as("identificacion","Ingrese identificacion");
+		$this->crud->display_as("observaciones","Ingrese las observaciones");
+		$this->crud->display_as("lugar_nacim","ciudad");
 		$this->crud->display_as("fechaingreso","Fecha de registro");
 		$this->crud->display_as("fechamodificacion","Ultima modificacion");
-		$this->crud->columns('nombre','apellido','telefono','email','direccion','ciudad','identificacion','observaciones');
+		$this->crud->columns("nombre","apellido","telefono","email","direccion","ciudad","identificacion","observaciones");
 		$this->crud->set_subject("Listado de pacientes");
 
 		//Aplicar el render  que es ejecutar estas variables y esperar los 3 componentes
