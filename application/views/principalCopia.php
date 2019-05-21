ace9cae7b18e740353c35950a18de3ead1b2be4f
sebastian <sebastiano-10@hotmail.com>
2019-05-20 20:56:37 -0500

    proceso de incluidos

---
+++ application/views/principalCopia.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+	<title>Bienvenidos al sistema principal</title>
+	<?php include('incluidos/css.php'); ?> 
+</head>
+<body class="nav-md">
+	<?php include('incluidos/menu_lateral.php'); ?>
+	<?php include('incluidos/menu_derecho.php'); ?>
+	<!-- page content -->
+	<div class="right_col" role="main">
+		<?php include('incluidos/aside.php');?>
+		<?php include('incluidos/caja1.php');?>
+		<br>
+		<?php include('incluidos/caja2.php');?>
+		<?php include('incluidos/caja3.php');?>
+		<?php include('incluidos/caja4.php');?>
+	
+
+	
+
+	<!--<p class="footer">Desarrollado por @DesarrollosSACV</p>-->
+	<?php include('incluidos/js.php'); ?>
+</body>
+</html>
